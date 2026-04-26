d0f6e49bf1c1bcc8a1b7949175cc1bb4bc3199f5
vansh-jain1704 <vanshjain0417@gmail.com>
2026-04-26 20:28:29 +0530

    Create App.js

---
+++ p6/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './App.css'; 
+import Counter from './CounterClass'; 
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <h1 className='head'>Welcome to the Counter App</h1> 
+      <Counter /> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
