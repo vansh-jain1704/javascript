0c6c93f55114e6fb99544cc4ccd8146eb4aa6eec
vansh-jain1704 <vanshjain0417@gmail.com>
2026-04-26 20:20:09 +0530

    Create script.js

---
+++ p3/script.js
@@ -0,0 +1,25 @@
+let student = { 
+  name: "Rahul", 
+  grade: 75, 
+  subjects: ["Math", "Science", "English"], 
+ 
+  displayInfo: function () { 
+    console.log("Name:", this.name); 
+    console.log("Grade:", this.grade); 
+    console.log("Subjects:", this.subjects); 
+  } 
+}; 
+ 
+student.displayInfo(); 
+ 
+// Add passed property 
+student.passed = student.grade >= 50; 
+ 
+console.log("Passed:", student.passed); 
+ 
+// Print only properties (not functions) 
+for (let key in student) { 
+  if (typeof student[key] !== "function") { 
+    console.log(key + " : " + student[key]); 
+  } 
+}
